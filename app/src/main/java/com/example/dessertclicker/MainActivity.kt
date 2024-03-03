b4fee3af76c6c19059370062c2dfd7cb90b34f16
paangulo6 <paulalardero581@gmail.com>
2024-03-03 13:50:59 +0100

    Acabada

--- app/src/main/java/com/example/dessertclicker/MainActivity.kt
+++ app/src/main/java/com/example/dessertclicker/MainActivity.kt
@@ -56,6 +56,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -153,7 +154,7 @@ private fun DessertClickerApp(
     desserts: List<Dessert>
 ) {
 
-    var revenue by remember { mutableStateOf(0) }
+    var revenue by rememberSaveable { mutableStateOf(0) }
     var dessertsSold by remember { mutableStateOf(0) }
 
     val currentDessertIndex by remember { mutableStateOf(0) }
